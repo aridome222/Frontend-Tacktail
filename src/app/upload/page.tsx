394b92617c83734c450d895fa013f897cbdf9d23
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-11-05 21:36:14 +0900

    update: 投稿に必要な入力フィールドを追加

--- src/app/upload/page.tsx
+++ src/app/upload/page.tsx
@@ -1,5 +1,36 @@
-import styles from "./upload.module.css";
+import styles from "./upload.module.css"; // CSSモジュールのインポート
 
 export default function Upload() {
-  return <div className={styles.page}>こんにちはなのだ！</div>;
+  return (
+    <div className={styles.page}>
+      <h1>カクテル投稿</h1>
+      <form className={styles.form}>
+        {/* 画像フィールド */}
+        <label>カクテルの画像:</label>
+
+        {/* カクテル名フィールド */}
+        <label>
+          カクテル名:
+          <input type="text" name="name" className={styles.input} />
+        </label>
+
+        {/* コメントフィールド */}
+        <label>
+          コメント:
+          <input type="text" name="comment" className={styles.input} />
+        </label>
+
+        {/* レシピフィールド */}
+        <label>
+          必要なレシピ:
+          <input type="text" name="text" className={styles.input} />
+        </label>
+
+        {/* 送信ボタン */}
+        <button type="submit" className={styles.button}>
+          投稿
+        </button>
+      </form>
+    </div>
+  );
 }
