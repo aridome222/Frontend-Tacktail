d265107e018f8d494cf1b42dc287c59b154f3179
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-11-06 16:22:39 +0900

    add: レシピページの追加

---
+++ src/app/recipe/page.tsx
@@ -0,0 +1,9 @@
+import styles from "./upload.module.css"; // CSSモジュールのインポート
+
+export default function Recipe() {
+  return (
+    <div className={styles.page}>
+      <h1>レシピページ</h1>
+    </div>
+  );
+}
