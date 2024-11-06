6904fac21ada9d8fcf710f4ffdfa33cfb5ec413d
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-06 16:48:00 +0900

    add: レシピ一覧のページとそこに配置するカードのコンポーネントをファイルだけ作成

---
+++ src/app/recipes/page.tsx
@@ -0,0 +1,12 @@
+import { Card } from "../components/Card"
+
+const Recipes = () => {
+    return (
+        <>
+            <h1>recipes</h1>
+            <Card />
+        </>
+    )
+}
+
+export default Recipes
