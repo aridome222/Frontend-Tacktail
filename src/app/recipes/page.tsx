7a3eee7765c5091904faca92df4cb29d2f38b730
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-06 20:10:35 +0900

    update: レシピを並べたレイアウトを確認するため、モックデータを用意し map 関数で配置するよう修正

--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,12 +1,30 @@
 import { Card } from "../components/Card"
+// モックデータ
+const recipesData = [
+    {
+        heading: "モヒート",
+        contents: ["ラム", "ミント", "ライム", "炭酸水"],
+    },
+    {
+        heading: "ジンバック",
+        contents: ["ジン", "レモン", "ジンジャーエール"],
+    },
+    {
+        heading: "ロングアイランドアイスティー",
+        contents: ["ジン", "ウォッカ", "レモン", "ホワイトラム", "テキーラ", "コアントロー", "コーラ"],
+    },
+];
 
 const Recipes = () => {
     return (
         <>
-            <h1>recipes</h1>
-            <Card />
+            <h1>レシピ一覧</h1>
+            {recipesData.map((recipe, index) => (
+                <Card key={index} heading={recipe.heading} contents={recipe.contents} />
+            ))}
         </>
-    )
-}
+    );
+};
 
-export default Recipes
+export default Recipes;
+
