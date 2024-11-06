39bef288ec108c801c9367ab776bb5b247be0b4e
KakinokiKanta <kanta12621@gmail.com>
2024-11-07 02:20:17 +0900

    add: ストーリーモード用のカードコンポーネントを追加

---
+++ src/app/story/_components/StoryCard.tsx
@@ -0,0 +1,9 @@
+import styles from "./StoryCard.module.css";
+
+export const StoryCard: React.FC = () => {
+  return (
+    <div className={styles.card}>
+      <p>container</p>
+    </div>
+  );
+};
