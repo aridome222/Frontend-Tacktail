59a1f6eea921db6f93dd0e89aefd4a53b9d48c25
KakinokiKanta <kanta12621@gmail.com>
2024-11-07 16:49:31 +0900

    add: カードの外観を実装

--- src/app/story/_components/StoryCard.tsx
+++ src/app/story/_components/StoryCard.tsx
@@ -1,9 +1,22 @@
+import Image from "next/image";
 import styles from "./StoryCard.module.css";
 
 export const StoryCard: React.FC = () => {
   return (
     <div className={styles.card}>
-      <p>モスコミュールを作ってみよう！</p>
+      <div className={styles.imageParent}>
+        <Image
+          className={styles.image}
+          src="/images/story_top.png"
+          alt="image of story mode"
+          width={100}
+          height={100}
+          priority
+        />
+      </div>
+      <div className={styles.text}>
+        <h2 className={styles.description}>モスコミュールを作ってみよう</h2>
+      </div>
     </div>
   );
 };
