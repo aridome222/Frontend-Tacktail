53e7949e1fe1ce727102b530ac3e3efc153ed4a7
KakinokiKanta <kanta12621@gmail.com>
2024-11-07 13:52:16 +0900

    add: ストーリーモード上部のタイトルコンポーネントを追加

---
+++ src/app/story/_components/StoryTop.tsx
@@ -0,0 +1,9 @@
+import styles from "./StoryTop.module.css";
+
+export const StoryTop: React.FC = () => {
+  return (
+    <div className={styles.top}>
+      <h1>Story Mode!</h1>
+    </div>
+  );
+}
