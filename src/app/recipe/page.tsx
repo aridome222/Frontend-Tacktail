74c867ed4dde5fcad7cce6a1d02f38578b073fe6
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-11-07 21:28:36 +0900

    update: 配置の調整＆レンジスライダーの作成

--- src/app/recipe/page.tsx
+++ src/app/recipe/page.tsx
@@ -5,58 +5,61 @@ import styles from "./recipe.module.css"; // CSSモジュールのインポー�
 
 const Recipe: React.FC = () => {
   return (
-    <div className={styles.page}>
-      <h1>カクテルレシピページ</h1>
+    <>
+      <div className={styles.page}>
+        <h1>カクテルレシピページ</h1>
 
-      {/* カクテルの基本情報 */}
-      <section>
-        <h2>カクテル名: モスコミュール</h2>
-        <p>調理時間: 約5分</p>
-        <p>費用目安: 500円程度</p>
-        <p>
-          説明:
-          モスコミュールはウォッカベースのカクテルで、ジンジャーエールとライムの爽やかな味わいが特徴です。
-        </p>
-      </section>
+        {/* カクテルの基本情報 */}
+        <div className={styles.description}>
+          <div className={styles.imageContainer}>
+            <img
+              src="/images/react.png"
+              alt="ステップ1の画像"
+              className={styles.image}
+            />
+          </div>
+          <div className={styles.text}>
+            <h2>モスコミュール</h2>
+            <p>費用目安: 500円程度</p>
+            <p>
+              説明:
+              モスコミュールはウォッカベースのカクテルで、ジンジャーエールとライムの爽やかな味わいが特徴です。
+            </p>
+          </div>
+        </div>
 
-      {/* 作り方の手順（画像付き） */}
-      <section>
-        {/* デモ画像（本番では適切な画像パスを指定） */}
-        <img
-          src="../../favicon.ico"
-          alt="ステップ1の画像"
-          className={styles.stepImage}
-        />
-      </section>
-
-      {/* 材料と比率 */}
-      <section>
+        {/* 材料と比率 */}
         <h3>材料と比率</h3>
-        <p>以下の比率で混ぜてください。</p>
-        <div>
-          <label htmlFor="vodka-slider">ウォッカ: 5/10</label>
-          <input
-            id="vodka-slider"
-            type="range"
-            min="0"
-            max="10"
-            step="1"
-            value={5}
-          />
+        <div className={styles.rangeSliderContainer}>
+          <div className={styles.rangeSliderItem}>
+            <label htmlFor="vodka-slider">ウォッカ</label>
+            0
+            <input
+              id="vodka-slider"
+              type="range"
+              min="0"
+              max="10"
+              step="1"
+              value={5}
+            />
+            10
+          </div>
+          <div className={styles.rangeSliderItem}>
+            <label htmlFor="orange-juice-slider">オレンジジュース</label>
+            0
+            <input
+              id="orange-juice-slider"
+              type="range"
+              min="0"
+              max="10"
+              step="1"
+              value={5}
+            />
+            10
+          </div>
         </div>
-        <div>
-          <label htmlFor="orange-juice-slider">オレンジジュース: 5/10</label>
-          <input
-            id="orange-juice-slider"
-            type="range"
-            min="0"
-            max="10"
-            step="1"
-            value={5}
-          />
-        </div>
-      </section>
-    </div>
+      </div>
+    </>
   );
 };
 
