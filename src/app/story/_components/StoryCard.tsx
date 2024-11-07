7c633b0d223f066c6982968e4f185c3a1fe2c60c
KakinokiKanta <kanta12621@gmail.com>
2024-11-07 17:39:11 +0900

    update: 複数カードの表示に成功

--- src/app/story/_components/StoryCard.tsx
+++ src/app/story/_components/StoryCard.tsx
@@ -2,25 +2,71 @@ import Image from "next/image";
 import styles from "./StoryCard.module.css";
 
 export const StoryCard: React.FC = () => {
+  // const MOCK_CLEAR = {
+  //   clear_number: 1,
+  // }
+
+  const MOCK_DATA_LIST = [
+    {
+      "title": "モスコミュールを作ってみよう",
+      "image": "/images/story_top.png",
+      "recipe": [
+        "ウォッカ", "ジンジャーエール"
+      ]
+    },
+    {
+      "title": "モスコミュールを作ってみよう",
+      "image": "/images/story_top.png",
+      "recipe": [
+        "ウォッカ", "ジンジャーエール"
+      ]
+    },
+    {
+      "title": "モスコミュールを作ってみよう",
+      "image": "/images/story_top.png",
+      "recipe": [
+        "ウォッカ", "ジンジャーエール"
+      ]
+    },
+    {
+      "title": "モスコミュールを作ってみよう",
+      "image": "/images/story_top.png",
+      "recipe": [
+        "ウォッカ", "ジンジャーエール"
+      ]
+    },
+  ]
+
   return (
-    <div className={styles.card}>
-      <div className={styles.imageParent}>
-        <Image
-          className={styles.image}
-          src="/images/story_top.png"
-          alt="image of story mode"
-          width={100}
-          height={100}
-          priority
-        />
-      </div>
-      <div className={styles.text}>
-        <p className={styles.description}>モスコミュールを作ってみよう</p>
-        <ul className={styles.list}>
-          <li>ウォッカ</li>
-          <li>ジンジャーエール</li>
-        </ul>
-      </div>
-    </div>
+    <>
+      {MOCK_DATA_LIST.map((item, index) => {
+        return (
+          <div className={styles.card} key={index}>
+            <div className={styles.imageParent}>
+              <Image
+                className={styles.image}
+                src={item.image}
+                alt="image of story mode"
+                width={100}
+                height={100}
+                priority
+              />
+            </div>
+            <div className={styles.text}>
+              <p className={styles.description}>{item.title}</p>
+              <ul className={styles.list}>
+                {item.recipe.map((matelial, index) => {
+                  return (
+                    <li key={index}>
+                      {matelial}
+                    </li>
+                  );
+                })}
+              </ul>
+            </div>
+          </div>
+        );
+      })}
+    </>
   );
 };
