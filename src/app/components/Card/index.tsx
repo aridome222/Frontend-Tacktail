7529ecb3ee6f376b0a93b3531e6f8c5bd7665717
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-07 18:33:51 +0900

    update: レシピのカードコンポーネントを作成し、レシピ一覧ページにモックデータを代入して配置

--- src/app/components/Card/index.tsx
+++ src/app/components/Card/index.tsx
@@ -1,21 +1,26 @@
-import Image from "next/image"
-import styles from "./Card.module.css"
+import type React from 'react';
+import { CocktailImage } from '../CocktailImage';
+import styles from './Card.module.css';
 
 type CardProps = {
-    heading: string;
-    contents: string[];
-}
+  heading: string;
+  contents: string[];
+};
 
 export const Card: React.FC<CardProps> = ({ heading, contents }: CardProps) => {
-    return (
-        <>
-            <div className={styles.parent}>
-                <Image className={styles.image} src="/images/robot_and_hogeta.jpeg" alt="hogeta" width={150} height={150} />
-                <div className={styles.mediaBody}>
-                    <h1 className={styles.heading}>{heading}</h1>
-                    {contents.map((item) => <p className={styles.contents} key={item}>{item}</p>)}
-                </div>
-            </div >
-        </>
-    )
-}
+  return (
+    <>
+      <div className={styles.parent}>
+        <CocktailImage backgroundImagePath='/images/robot_and_hogeta.jpeg' cocktailName={heading} />
+        <div className={styles.mediaBody}>
+          <h1 className={styles.heading}>{heading}</h1>
+          {contents.map((item) => (
+            <p className={styles.contents} key={item}>
+              {item}
+            </p>
+          ))}
+        </div>
+      </div>
+    </>
+  );
+};
--- src/app/recipes/page.tsx
+++ src/app/recipes/page.tsx
@@ -1,30 +1,36 @@
-import { Card } from "../components/Card"
+import { Card } from '../components/Card';
+import styles from './Recipes.module.css';
+
 // モックデータ
 const recipesData = [
-    {
-        heading: "モヒート",
-        contents: ["ラム", "ミント", "ライム", "炭酸水"],
-    },
-    {
-        heading: "ジンバック",
-        contents: ["ジン", "レモン", "ジンジャーエール"],
-    },
-    {
-        heading: "ロングアイランドアイスティー",
-        contents: ["ジン", "ウォッカ", "レモン", "ホワイトラム", "テキーラ", "コアントロー", "コーラ"],
-    },
+  {
+    id: 0,
+    heading: 'モヒート',
+    contents: ['ラム', 'ミント', 'ライム', '炭酸水'],
+  },
+  {
+    id: 1,
+    heading: 'ジンバック',
+    contents: ['ジン', 'レモン', 'ジンジャーエール'],
+  },
+  {
+    id: 2,
+    heading: 'ロングアイランドアイスティー',
+    contents: ['ジン', 'ウォッカ', 'ホワイトラム', 'テキーラ', 'コアントロー', 'コーラ'],
+  },
 ];
 
 const Recipes = () => {
-    return (
-        <>
-            <h1>レシピ一覧</h1>
-            {recipesData.map((recipe, index) => (
-                <Card key={index} heading={recipe.heading} contents={recipe.contents} />
-            ))}
-        </>
-    );
+  return (
+    <>
+      <h1>レシピ一覧</h1>
+      <div className={styles.cardList}>
+        {recipesData.map((recipe) => (
+          <Card key={recipe.id} heading={recipe.heading} contents={recipe.contents} />
+        ))}
+      </div>
+    </>
+  );
 };
 
 export default Recipes;
-
