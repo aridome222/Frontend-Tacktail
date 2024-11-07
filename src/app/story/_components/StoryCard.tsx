047974b31f01906ec1319f8b93d4c893e7c922f6
KakinokiKanta <kanta12621@gmail.com>
2024-11-07 21:27:50 +0900

    fix: clearロゴをスクロール対応

--- src/app/story/_components/StoryCard.tsx
+++ src/app/story/_components/StoryCard.tsx
@@ -13,36 +13,34 @@ export const StoryCard: React.FC<StoryCardProps> = (props) => {
   const MAX_MATERIAL = 3;
 
   return (
-    <div className={`${props.isActive ? styles.active : styles.deactive}`}>
-      <div className={styles.card}>
-        <div className={styles.imageParent}>
-          <Image
-            className={styles.image}
-            src={props.image}
-            alt="cocktail image"
-            width={100}
-            height={100}
-            priority
-          />
-        </div>
-        <div className={styles.text}>
-          <p className={styles.description}>{props.title}</p>
-          <div className={styles.listParent}>
-            <ul className={styles.list}>
-              {props.recipe.map((material, index) => {
-                return (
-                  <>
-                    {index < MAX_MATERIAL &&
-                      <li className={styles.material} key={index}>
-                        {material}
-                      </li>
-                    }
-                  </>
-                );
-              })}
-            </ul>
-            {props.recipe.length >= MAX_MATERIAL && <span className={styles.etc}>etc</span>}
-          </div>
+    <div className={`${styles.card} ${props.isActive ? styles.active : styles.deactive}`}>
+      <div className={styles.imageParent}>
+        <Image
+          className={styles.image}
+          src={props.image}
+          alt="cocktail image"
+          width={100}
+          height={100}
+          priority
+        />
+      </div>
+      <div className={styles.text}>
+        <p className={styles.description}>{props.title}</p>
+        <div className={styles.listParent}>
+          <ul className={styles.list}>
+            {props.recipe.map((material, index) => {
+              return (
+                <>
+                  {index < MAX_MATERIAL &&
+                    <li className={styles.material} key={index}>
+                      {material}
+                    </li>
+                  }
+                </>
+              );
+            })}
+          </ul>
+          {props.recipe.length >= MAX_MATERIAL && <span className={styles.etc}>etc</span>}
         </div>
       </div>
       {props.isClear &&
