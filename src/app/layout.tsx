a24427e7543ad580ce3c18ba9dc3b6a1a388af80
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-07 22:42:17 +0900

    update: 全ページにヘッダーを置くため、layout.tsx にヘッダーを追加

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next';
 import localFont from 'next/font/local';
 import type React from 'react';
 import './globals.css';
+import { Header } from './components/Header';
 
 const geistSans = localFont({
   src: './fonts/GeistVF.woff',
@@ -22,6 +23,7 @@ export default function RootLayout({
   return (
     <html lang='ja'>
       <body className={`${geistSans.variable}`}>
+        <Header />
         <main>{children}</main>
       </body>
     </html>
