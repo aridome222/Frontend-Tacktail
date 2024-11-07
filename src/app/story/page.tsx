d44428c508b923d73c47340d4af3af4492af5229
KakinokiKanta <kanta12621@gmail.com>
2024-11-07 20:11:58 +0900

    update: いったんのUI完成

--- src/app/story/page.tsx
+++ src/app/story/page.tsx
@@ -7,28 +7,29 @@ const Story: React.FC = () => {
   const MOCK_DATA_LIST = [
     {
       "title": "モスコミュールを作ってみよう",
-      "image": "/images/story_top.png",
+      "image": "/images/cocktail/moscow_mule.jpg",
       "recipe": [
         "ウォッカ", "ジンジャーエール"
-      ]
+      ],
+      "story": 2
     },
     {
       "title": "スクリュードライバーを作ってみよう",
-      "image": "/images/story_top.png",
+      "image": "/images/cocktail/screw_driver.jpg",
       "recipe": [
         "ウォッカ", "オレンジジュース"
       ]
     },
     {
       "title": "ジンバックを作ってみよう",
-      "image": "/images/story_top.png",
+      "image": "/images/cocktail/gin_buck.jpg",
       "recipe": [
         "ジン", "ジンジャーエール"
       ]
     },
     {
       "title": "ロングアイランドアイスティーを作ってみよう",
-      "image": "/images/story_top.png",
+      "image": "/images/cocktail/long_island_ice_tea.jpg",
       "recipe": [
         "ジン", "ウォッカ", "テキーラ", "ラム", "コアントロー", "コーラ"
       ]
