c486549684151f931886aa949b82ccc00a9e079b
KakinokiKanta <kanta12621@gmail.com>
2024-11-07 14:25:59 +0900

    add: Imageコンポーネントを追加

--- src/app/story/_components/StoryTop.tsx
+++ src/app/story/_components/StoryTop.tsx
@@ -1,9 +1,21 @@
+import Image from "next/image";
 import styles from "./StoryTop.module.css";
 
 export const StoryTop: React.FC = () => {
   return (
     <div className={styles.top}>
-      <h1>Story Mode!</h1>
+      <div className={styles.text}>
+        <h1>Story Mode!</h1>
+      </div>
+      <div className={styles.imageParent}>
+        <Image
+          className={styles.image}
+          src="/images/story_top.png"
+          alt="image of story mode"
+          fill
+          priority
+        />
+      </div>
     </div>
   );
 }
