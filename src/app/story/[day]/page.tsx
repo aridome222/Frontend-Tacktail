cd79ae6182dfdbbe294a3b7078bd7a5d2d48d248
KakinokiKanta <kanta12621@gmail.com>
2024-11-08 15:39:58 +0900

    add: 動的ルーティングなページを追加

---
+++ src/app/story/[day]/page.tsx
@@ -0,0 +1,17 @@
+import type React from 'react';
+
+const StoryDay: React.FC = () => {
+  return (
+    <section>
+      <div>
+        <h2>以下の材料を揃えよう！</h2>
+        <ul>
+          <li>ウォッカ</li>
+          <li>ジンジャーエール</li>
+        </ul>
+      </div>
+    </section>
+  );
+};
+
+export default StoryDay;
