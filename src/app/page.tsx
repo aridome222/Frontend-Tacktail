5778f3ae81dda0b6efc11bf8d8381e921a93de6d
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-11-09 19:39:06 +0900

    update: 適当にトップページを作成

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,53 @@
-import styles from './page.module.css';
+import styles from "./page.module.css";
+import { Card } from "./components/Card";
+
+// モックデータ
+const recipesData = [
+  {
+    id: 0,
+    image: "/images/cocktail/screw_driver.jpg",
+    cocktail: "モヒート",
+    contents: ["ラム", "ミント", "ライム", "炭酸水"],
+  },
+  {
+    id: 1,
+    image: "/images/cocktail/screw_driver.jpg",
+    cocktail: "ジンバック",
+    contents: ["ジン", "レモン", "ジンジャーエール"],
+  },
+  {
+    id: 2,
+    image: "/images/cocktail/screw_driver.jpg",
+    cocktail: "ロングアイランドアイスティー",
+    contents: [
+      "ジン",
+      "ウォッカ",
+      "ホワイトラム",
+      "テキーラ",
+      "コアントロー",
+      "コーラ",
+    ],
+  },
+];
 
 export default function Home() {
   return (
-    <div>
-      <h1 className={styles.text}>こんにちはなのだ！</h1>
-    </div>
+    <>
+      <div className={styles.page}>
+        <h1 className={styles.text}>トップページ</h1>
+        <h2>オススメ！</h2>
+        <div className={styles.cardList}>
+          {recipesData.map((recipe) => (
+            <Card
+              key={recipe.id}
+              id={recipe.id}
+              image={recipe.image}
+              cocktail={recipe.cocktail}
+              contents={recipe.contents}
+            />
+          ))}
+        </div>
+      </div>
+    </>
   );
 }
