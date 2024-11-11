8f661072dcaf7e35b931513cc6a1ed76084b54a1
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-11-12 06:12:50 +0900

    update: 初心者には「ビター系」のお酒など直感的に分かりにくいため、ユーザーのレベルに合わせてオススメを表示

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,6 @@
 import styles from "./page.module.css";
 import { Card } from "./components/Card";
+import Image from "next/image";
 
 // モックデータ
 const recipesData = [
@@ -8,91 +9,55 @@ const recipesData = [
     image: "/images/cocktail/screw_driver.jpg",
     cocktail: "モヒート",
     contents: ["ラム", "ミント", "ライム", "炭酸水"],
-    category: "フルーティー系",
+    level: "beginner",
   },
   {
     id: 1,
     image: "/images/cocktail/screw_driver.jpg",
     cocktail: "ジン・トニック",
     contents: ["ジン", "トニックウォーター"],
-    category: "ドライ系",
+    level: "beginner",
   },
   {
     id: 2,
     image: "/images/cocktail/screw_driver.jpg",
     cocktail: "ウイスキーサワー",
     contents: ["ウイスキー", "レモンジュース", "シロップ"],
-    category: "サワー系",
+    level: "advanced",
   },
   {
     id: 3,
     image: "/images/cocktail/screw_driver.jpg",
     cocktail: "ネグローニ",
     contents: ["ジン", "カンパリ", "スイート・ベルモット"],
-    category: "ビター系",
+    level: "advanced",
   },
   {
     id: 4,
     image: "/images/cocktail/screw_driver.jpg",
-    cocktail: "マルガリータ",
-    contents: ["テキーラ", "ライムジュース", "オレンジリキュール"],
-    category: "サワー系",
+    cocktail: "ドライマティーニ",
+    contents: ["ジン", "ドライ・ベルモット"],
+    level: "advanced",
   },
   {
     id: 5,
     image: "/images/cocktail/screw_driver.jpg",
-    cocktail: "カンパリソーダ",
-    contents: ["カンパリ", "ソーダ"],
-    category: "ビター系",
-  },
-  {
-    id: 6,
-    image: "/images/cocktail/screw_driver.jpg",
-    cocktail: "ドライマティーニ",
-    contents: ["ジン", "ドライ・ベルモット"],
-    category: "ドライ系",
-  },
-  {
-    id: 7,
-    image: "/images/cocktail/screw_driver.jpg",
     cocktail: "スパイシーマルガリータ",
     contents: ["テキーラ", "ライムジュース", "チリパウダー"],
-    category: "スパイシー系",
+    level: "advanced",
   },
 ];
 
 export default function Home() {
-  // 各カテゴリをグループ化
+  // 初心者向けと上級者向けのグループ化
   const categories = [
     {
-      name: "ドライ系",
-      categoryData: recipesData.filter(
-        (recipe) => recipe.category === "ドライ系"
-      ),
+      name: "まずは飲みやすい一杯から！初心者向け",
+      categoryData: recipesData.filter((recipe) => recipe.level === "beginner"),
     },
     {
-      name: "サワー系",
-      categoryData: recipesData.filter(
-        (recipe) => recipe.category === "サワー系"
-      ),
-    },
-    {
-      name: "ビター系",
-      categoryData: recipesData.filter(
-        (recipe) => recipe.category === "ビター系"
-      ),
-    },
-    {
-      name: "フルーティー系",
-      categoryData: recipesData.filter(
-        (recipe) => recipe.category === "フルーティー系"
-      ),
-    },
-    {
-      name: "スパイシー系",
-      categoryData: recipesData.filter(
-        (recipe) => recipe.category === "スパイシー系"
-      ),
+      name: "カクテルアワード優勝への道！上級者向け",
+      categoryData: recipesData.filter((recipe) => recipe.level === "advanced"),
     },
   ];
 
@@ -102,11 +67,11 @@ export default function Home() {
         <h1 className={styles.h1}>トップページ</h1>
         <h2 className={styles.h2}>オススメのカクテル</h2>
 
-        {/* 各カテゴリごとにレシピを表示 */}
+        {/* 初心者向けと上級者向けにレシピを表示 */}
         <div className={styles.categoryList}>
           {categories.map((category) => (
             <div key={category.name}>
-              <h3>{category.name}</h3>
+              <h3 className={styles.margin_bottom}>{category.name}</h3>
               <div className={styles.cardList}>
                 {category.categoryData.map((recipe) => (
                   <Card
