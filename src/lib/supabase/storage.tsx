5b9a7a3b9af93fd7650ca50b9cfe882f4e7cef23
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-12 21:11:23 +0900

    update: Supabase ストレージからファイルをダウンロードする処理を追加

--- src/lib/supabase/storage.tsx
+++ src/lib/supabase/storage.tsx
@@ -1,22 +1,19 @@
 import supabase from '../../utils/supabase';
 
-type uploadStorageProps = {
+type storageProps = {
   file: File;
   bucketName: string;
   cocktailId: number;
   username: string;
 };
 
-type uploadPathname = {
-  path: string;
-};
-
+// Supabase のストレージへファイルをアップロードする処理
 export const uploadStorage = async ({
   file,
   bucketName,
   cocktailId,
   username,
-}: uploadStorageProps): Promise<uploadPathname> => {
+}: storageProps): Promise<string> => {
   // 引数に与えた username と cocktailId で誰の何のカクテルかを判別する。
   // TODO: アップロードされた画像と同じ拡張子でアップロードする処理の実装
   const pathName = `cocktails/${username}_${String(cocktailId)}.jpeg`;
@@ -24,9 +21,27 @@ export const uploadStorage = async ({
     cacheControl: '3600',
     upsert: true,
   });
-  console.log(data);
   if (error) throw error;
-  return {
-    path: data?.path ?? null,
-  };
+  return data?.path ?? '';
+};
+
+// Supabase のストレージからファイルをダウンロードする処理
+export const downloadStorage = async ({
+  bucketName,
+  cocktailId,
+  username,
+}: Omit<storageProps, 'file'>): Promise<string | null> => {
+  const pathName = `cocktails/${username}_${String(cocktailId)}.jpeg`;
+  const { data, error } = await supabase.storage.from(bucketName).download(pathName);
+
+  if (error) throw new Error(`Failed to download file: ${error.message}`);
+
+  // ストレージにファイルが存在する場合は、ダウロードしたファイルの一時的な URL を返す
+  if (data) {
+    console.log(URL.createObjectURL(data));
+    return URL.createObjectURL(data);
+  }
+
+  // ストレージにファイルが存在しない場合
+  return null;
 };
