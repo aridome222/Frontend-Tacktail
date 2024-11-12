47827e3d981d27b41f0727b4a24ab4870f9f275a
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-11-12 18:35:46 +0900

    refactor: biomeのlintとformatに通るよう修正

--- src/app/components/CocktailImage/index.tsx
+++ src/app/components/CocktailImage/index.tsx
@@ -1,6 +1,6 @@
-import Image from "next/image";
-import type React from "react";
-import styles from "./CocktailImage.module.css";
+import Image from 'next/image';
+import type React from 'react';
+import styles from './CocktailImage.module.css';
 
 type CocktailImageProps = {
   backgroundImagePath: string;
@@ -15,20 +15,15 @@ export const CocktailImage: React.FC<CocktailImageProps> = ({
     {/* メインの背景画像 */}
     <Image
       src={backgroundImagePath}
-      alt="Background"
+      alt='Background'
       width={100}
       height={100}
-      style={{ objectFit: "cover" }}
+      style={{ objectFit: 'cover' }}
     />
 
     {/* メイン画像の右斜め上に配置する画像 */}
     <div className={styles.overlay}>
-      <Image
-        src="/images/golden_ribbon.png"
-        alt="Overlay"
-        width={400}
-        height={50}
-      />
+      <Image src='/images/golden_ribbon.png' alt='Overlay' width={400} height={50} />
       <p className={styles.cocktailName}>{cocktailName}</p>
     </div>
   </div>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,49 @@
-import styles from "./page.module.css";
-import { Card } from "./components/Card";
-import Image from "next/image";
-
+import Image from 'next/image';
+import { Card } from './components/Card';
+import styles from './page.module.css';
 // モックデータ
 const recipesData = [
   {
     id: 0,
-    image: "/images/cocktail/screw_driver.jpg",
-    cocktail: "モヒート",
-    contents: ["ラム", "ミント", "ライム", "炭酸水"],
-    level: "beginner",
+    image: '/images/cocktail/screw_driver.jpg',
+    cocktail: 'モヒート',
+    contents: ['ラム', 'ミント', 'ライム', '炭酸水'],
+    level: 'beginner',
   },
   {
     id: 1,
-    image: "/images/cocktail/screw_driver.jpg",
-    cocktail: "ジン・トニック",
-    contents: ["ジン", "トニックウォーター"],
-    level: "beginner",
+    image: '/images/cocktail/screw_driver.jpg',
+    cocktail: 'ジン・トニック',
+    contents: ['ジン', 'トニックウォーター'],
+    level: 'beginner',
   },
   {
     id: 2,
-    image: "/images/cocktail/screw_driver.jpg",
-    cocktail: "ウイスキーサワー",
-    contents: ["ウイスキー", "レモンジュース", "シロップ"],
-    level: "advanced",
+    image: '/images/cocktail/screw_driver.jpg',
+    cocktail: 'ウイスキーサワー',
+    contents: ['ウイスキー', 'レモンジュース', 'シロップ'],
+    level: 'advanced',
   },
   {
     id: 3,
-    image: "/images/cocktail/screw_driver.jpg",
-    cocktail: "ネグローニ",
-    contents: ["ジン", "カンパリ", "スイート・ベルモット"],
-    level: "advanced",
+    image: '/images/cocktail/screw_driver.jpg',
+    cocktail: 'ネグローニ',
+    contents: ['ジン', 'カンパリ', 'スイート・ベルモット'],
+    level: 'advanced',
   },
   {
     id: 4,
-    image: "/images/cocktail/screw_driver.jpg",
-    cocktail: "ドライマティーニ",
-    contents: ["ジン", "ドライ・ベルモット"],
-    level: "advanced",
+    image: '/images/cocktail/screw_driver.jpg',
+    cocktail: 'ドライマティーニ',
+    contents: ['ジン', 'ドライ・ベルモット'],
+    level: 'advanced',
   },
   {
     id: 5,
-    image: "/images/cocktail/screw_driver.jpg",
-    cocktail: "スパイシーマルガリータ",
-    contents: ["テキーラ", "ライムジュース", "チリパウダー"],
-    level: "advanced",
+    image: '/images/cocktail/screw_driver.jpg',
+    cocktail: 'スパイシーマルガリータ',
+    contents: ['テキーラ', 'ライムジュース', 'チリパウダー'],
+    level: 'advanced',
   },
 ];
 
@@ -52,12 +51,12 @@ export default function Home() {
   // 初心者向けと上級者向けのグループ化
   const categories = [
     {
-      name: "まずは飲みやすい一杯から！初心者向け",
-      categoryData: recipesData.filter((recipe) => recipe.level === "beginner"),
+      name: 'まずは飲みやすい一杯から！初心者向け',
+      categoryData: recipesData.filter((recipe) => recipe.level === 'beginner'),
     },
     {
-      name: "カクテルアワード優勝への道！上級者向け",
-      categoryData: recipesData.filter((recipe) => recipe.level === "advanced"),
+      name: 'カクテルアワード優勝への道！上級者向け',
+      categoryData: recipesData.filter((recipe) => recipe.level === 'advanced'),
     },
   ];
 
@@ -66,12 +65,7 @@ export default function Home() {
       <div className={styles.page}>
         {/* トップページの画像 */}
         <div className={styles.imageContainer}>
-          <Image
-            src="/images/cocktail/topPage_bar.jpg"
-            alt="topPage_bar"
-            fill
-            priority
-          ></Image>
+          <Image src='/images/cocktail/topPage_bar.jpg' alt='topPage_bar' fill priority />
           <p className={styles.overlayText}>Tacktailへようこそ</p>
         </div>
 
@@ -88,12 +82,8 @@ export default function Home() {
           </div>
           <div className={styles.margin_y}>
             <p className={styles.text_larger}>そんなあなたに！</p>
-            <p>
-              Tacktailはカクテル選びの手間を減らし、ゲーム感覚でカクテルの物知りになれます！
-            </p>
-            <p>
-              自宅で楽しみながら、自分だけのカクテルコレクションを作ってみましょう！
-            </p>
+            <p>Tacktailはカクテル選びの手間を減らし、ゲーム感覚でカクテルの物知りになれます！</p>
+            <p>自宅で楽しみながら、自分だけのカクテルコレクションを作ってみましょう！</p>
           </div>
         </div>
 
