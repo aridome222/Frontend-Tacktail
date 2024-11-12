08271a4d6872c7926c3d4324ed4f234c85e6650e
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-13 02:38:56 +0900

    refactor: ビルドエラー解消のため、未使用パッケージのインポートを削除

--- src/lib/firebase/firebase.ts
+++ src/lib/firebase/firebase.ts
@@ -1,5 +1,5 @@
 import { initializeApp } from 'firebase/app';
-import { GoogleAuthProvider, getAuth } from 'firebase/auth/web-extension';
+import { GoogleAuthProvider } from 'firebase/auth/web-extension';
 import { getStorage } from 'firebase/storage';
 
 // Your web app's Firebase configuration
