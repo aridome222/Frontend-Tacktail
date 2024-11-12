00b1a5754afd4511aae34dc30657d375a4570ad7
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-11-12 18:35:46 +0900

    refactor: biomeのlintとformatに通るよう修正

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,50 +1,50 @@
-import Image from 'next/image';
-import { Card } from './components/Card';
-import styles from './page.module.css';
+import Image from "next/image";
+import { Card } from "./components/Card";
+import styles from "./page.module.css";
 
 // モックデータ
 const recipesData = [
   {
     id: 0,
-    image: '/images/cocktail/screw_driver.jpg',
-    cocktail: 'モヒート',
-    contents: ['ラム', 'ミント', 'ライム', '炭酸水'],
-    level: 'beginner',
+    image: "/images/cocktail/screw_driver.jpg",
+    cocktail: "モヒート",
+    contents: ["ラム", "ミント", "ライム", "炭酸水"],
+    level: "beginner",
   },
   {
     id: 1,
-    image: '/images/cocktail/screw_driver.jpg',
-    cocktail: 'ジン・トニック',
-    contents: ['ジン', 'トニックウォーター'],
-    level: 'beginner',
+    image: "/images/cocktail/screw_driver.jpg",
+    cocktail: "ジン・トニック",
+    contents: ["ジン", "トニックウォーター"],
+    level: "beginner",
   },
   {
     id: 2,
-    image: '/images/cocktail/screw_driver.jpg',
-    cocktail: 'ウイスキーサワー',
-    contents: ['ウイスキー', 'レモンジュース', 'シロップ'],
-    level: 'advanced',
+    image: "/images/cocktail/screw_driver.jpg",
+    cocktail: "ウイスキーサワー",
+    contents: ["ウイスキー", "レモンジュース", "シロップ"],
+    level: "advanced",
   },
   {
     id: 3,
-    image: '/images/cocktail/screw_driver.jpg',
-    cocktail: 'ネグローニ',
-    contents: ['ジン', 'カンパリ', 'スイート・ベルモット'],
-    level: 'advanced',
+    image: "/images/cocktail/screw_driver.jpg",
+    cocktail: "ネグローニ",
+    contents: ["ジン", "カンパリ", "スイート・ベルモット"],
+    level: "advanced",
   },
   {
     id: 4,
-    image: '/images/cocktail/screw_driver.jpg',
-    cocktail: 'ドライマティーニ',
-    contents: ['ジン', 'ドライ・ベルモット'],
-    level: 'advanced',
+    image: "/images/cocktail/screw_driver.jpg",
+    cocktail: "ドライマティーニ",
+    contents: ["ジン", "ドライ・ベルモット"],
+    level: "advanced",
   },
   {
     id: 5,
-    image: '/images/cocktail/screw_driver.jpg',
-    cocktail: 'スパイシーマルガリータ',
-    contents: ['テキーラ', 'ライムジュース', 'チリパウダー'],
-    level: 'advanced',
+    image: "/images/cocktail/screw_driver.jpg",
+    cocktail: "スパイシーマルガリータ",
+    contents: ["テキーラ", "ライムジュース", "チリパウダー"],
+    level: "advanced",
   },
 ];
 
@@ -52,12 +52,12 @@ export default function Home() {
   // 初心者向けと上級者向けのグループ化
   const categories = [
     {
-      name: 'まずは飲みやすい一杯から！初心者向け',
-      categoryData: recipesData.filter((recipe) => recipe.level === 'beginner'),
+      name: "まずは飲みやすい一杯から！初心者向け",
+      categoryData: recipesData.filter((recipe) => recipe.level === "beginner"),
     },
     {
-      name: 'カクテルアワード優勝への道！上級者向け',
-      categoryData: recipesData.filter((recipe) => recipe.level === 'advanced'),
+      name: "カクテルアワード優勝への道！上級者向け",
+      categoryData: recipesData.filter((recipe) => recipe.level === "advanced"),
     },
   ];
 
