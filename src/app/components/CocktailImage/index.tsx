201e696680631fa15a3b9ce1e9c5bd32126ff00f
aridome222 <hm20002@student.miyazaki-u.ac.jp>
2024-11-12 17:01:23 +0900

    refactor: 警告対応のため、非推奨の'objectFit'プロパティを推奨形式のインラインスタイルに変更

--- src/app/components/CocktailImage/index.tsx
+++ src/app/components/CocktailImage/index.tsx
@@ -1,6 +1,6 @@
-import Image from 'next/image';
-import type React from 'react';
-import styles from './CocktailImage.module.css';
+import Image from "next/image";
+import type React from "react";
+import styles from "./CocktailImage.module.css";
 
 type CocktailImageProps = {
   backgroundImagePath: string;
@@ -13,11 +13,22 @@ export const CocktailImage: React.FC<CocktailImageProps> = ({
 }: CocktailImageProps) => (
   <div className={styles.container}>
     {/* メインの背景画像 */}
-    <Image src={backgroundImagePath} alt='Background' width={100} height={100} objectFit='cover' />
+    <Image
+      src={backgroundImagePath}
+      alt="Background"
+      width={100}
+      height={100}
+      style={{ objectFit: "cover" }}
+    />
 
     {/* メイン画像の右斜め上に配置する画像 */}
     <div className={styles.overlay}>
-      <Image src='/images/golden_ribbon.png' alt='Overlay' width={400} height={50} />
+      <Image
+        src="/images/golden_ribbon.png"
+        alt="Overlay"
+        width={400}
+        height={50}
+      />
       <p className={styles.cocktailName}>{cocktailName}</p>
     </div>
   </div>
