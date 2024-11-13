934ecba482958eb2221d571eb07877cae3ac33dc
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-13 23:23:47 +0900

    update: ログイン状態保持に失敗しており、Auth.js 導入のため、途中経過をコミット

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,29 +1,6 @@
 // Server Actions の戻り値には Response 型が使えない
 'use server';
-
-export async function login(username: string, password: string): Promise<boolean> {
-  try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      body: JSON.stringify({ name: username, password: password }),
-    });
-
-    console.log(response);
-
-    // HTTPステータスが 200 でない場合は失敗
-    if (!response.ok) {
-      throw new Error('ログインに失敗しました');
-    }
-
-    return true;
-  } catch (error) {
-    console.error(error);
-    return false;
-  }
-}
+import { cookies } from 'next/headers';
 
 export async function signup(username: string, password: string): Promise<boolean> {
   try {
@@ -41,7 +18,65 @@ export async function signup(username: string, password: string): Promise<boolea
     }
 
     return true;
-  } catch (error) {
+  } catch (error: unknown) {
+    console.error(error);
+    return false;
+  }
+}
+
+export async function login(username: string, password: string): Promise<boolean> {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/login`, {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      mode: 'cors',
+      credentials: 'include',
+      body: JSON.stringify({ name: username, password: password }),
+    });
+
+    console.log(response);
+
+    const cookieStore = await cookies();
+    const theme = cookieStore.get('theme');
+
+    console.log(theme);
+
+    // HTTPステータスが 200 でない場合は失敗
+    if (!response.ok) {
+      throw new Error('ログインに失敗しました');
+    }
+
+    return true;
+  } catch (error: unknown) {
+    console.error(error);
+    return false;
+  }
+}
+
+export async function fetchLoginStatus(): Promise<boolean> {
+  try {
+    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`, {
+      method: 'GET',
+      mode: 'cors',
+      credentials: 'include',
+    });
+
+    console.log(response);
+
+    const cookieStore = await cookies();
+    const theme = cookieStore.get('theme');
+
+    console.log(theme);
+
+    // HTTPステータスが 200 でない場合は失敗
+    if (!response.ok) {
+      throw new Error('ログイン状態の取得に失敗しました');
+    }
+
+    return true;
+  } catch (error: unknown) {
     console.error(error);
     return false;
   }
