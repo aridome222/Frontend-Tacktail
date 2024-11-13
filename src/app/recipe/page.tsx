4f9d5af85c552bbcadffe0f95c4ae048955e7da4
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-13 16:52:51 +0900

    refactor: コンポーネントのファイル名が page.tsx になっていたため、index.tsx に変更

--- src/app/recipe/page.tsx
+++ src/app/recipe/page.tsx
@@ -1,5 +1,5 @@
 import type React from 'react';
-import { SendImage } from '../components/SendImage/page';
+import { SendImage } from '../components/SendImage/index';
 import { RangeSlider } from './_components/RangeSlider';
 import styles from './recipe.module.css';
 
