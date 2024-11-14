8f06cde9257f6d93623695fac18754f04673a240
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-14 21:34:35 +0900

    update: ログインを Auth.js による認証に切り替え成功！

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -1,32 +1,44 @@
 'use client';
 
-import { login } from '@/lib/auth';
 import Link from 'next/link';
 import { useRouter } from 'next/navigation';
 import type React from 'react';
 import { useState } from 'react';
-import type { FormEvent } from 'react';
 import styles from './Login.module.css';
+import { signIn } from 'next-auth/react';
 
-const Login = () => {
-  const [username, setUsername] = useState('');
-  const [password, setPassword] = useState('');
+const Login: React.FC = () => {
   const [error, setError] = useState('');
   const router = useRouter();
 
-  const handleSubmit = async (event: FormEvent<HTMLFormElement>) => {
+  const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
     event.preventDefault();
+
+    const data = new FormData(event.currentTarget);
+    const username = data.get('username') as string;
+    const password = data.get('password') as string;
 
     if (!username || !password) {
       setError('ユーザー名とパスワードを入力してください。');
       return;
     }
 
-    const isLoginSucceeded = await login(username, password);
-    if (isLoginSucceeded) {
+    try {
+      const result = await signIn('credentials', {
+        redirect: false,
+        username,
+        password,
+      });
+
+      if (result?.error) {
+        setError('ログインに失敗しました。ユーザー名とパスワードを確認してください。');
+        return;
+      }
+
       router.push('/');
-    } else {
-      setError('ログインに失敗しました。ユーザー名とパスワードを確認してください。');
+    } catch {
+      setError('ログインエラー。');
+      return;
     }
   };
 
@@ -44,24 +56,19 @@ const Login = () => {
           <div className={styles.inputParent}>
             <input
               id='username'
+              name='username'
               className={styles.input}
               placeholder='ユーザー名'
-              value={username}
-              onChange={(e) => setUsername(e.target.value)}
             />
             <input
               id='password'
+              name='password'
               type='password'
               className={styles.input}
               placeholder='パスワード'
-              value={password}
-              onChange={(e) => setPassword(e.target.value)}
             />
           </div>
           <div className={styles.linksContainer}>
-            <Link href='/' className={styles.forgotPasswordLink}>
-              パスワードを忘れた方はこちら
-            </Link>
             <Link href='/signup' className={styles.signupLink}>
               はじめてのご来店はこちら
             </Link>
