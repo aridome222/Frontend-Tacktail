419011c49fd946c899b98b839e17518a5029d8ce
YuyaKimura <hm20020@student.miyazaki-u.ac.jp>
2024-11-14 21:10:59 +0900

    update: auth.js のファイルへログイン処理を移動させたため、移動元ファイルの関数を削除

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,6 +1,5 @@
 // Server Actions の戻り値には Response 型が使えない
 'use server';
-import { cookies } from 'next/headers';
 
 export async function signup(username: string, password: string): Promise<boolean> {
   try {
@@ -12,72 +11,13 @@ export async function signup(username: string, password: string): Promise<boolea
       body: JSON.stringify({ name: username, password: password }),
     });
 
-    // HTTPステータスが 200 でない場合は失敗
-    if (!response.ok) {
-      throw new Error('ログインに失敗しました');
+    // HTTPステータスが 201 でない場合は失敗
+    if (!(response.status === 201)) {
+      throw new Error('ユーザー登録に失敗しました');
     }
 
     return true;
-  } catch (error: unknown) {
-    console.error(error);
-    return false;
-  }
-}
-
-export async function login(username: string, password: string): Promise<boolean> {
-  try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/login`, {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json',
-      },
-      mode: 'cors',
-      credentials: 'include',
-      body: JSON.stringify({ name: username, password: password }),
-    });
-
-    console.log(response);
-
-    const cookieStore = await cookies();
-    const theme = cookieStore.get('theme');
-
-    console.log(theme);
-
-    // HTTPステータスが 200 でない場合は失敗
-    if (!response.ok) {
-      throw new Error('ログインに失敗しました');
-    }
-
-    return true;
-  } catch (error: unknown) {
-    console.error(error);
-    return false;
-  }
-}
-
-export async function fetchLoginStatus(): Promise<boolean> {
-  try {
-    const response = await fetch(`${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/login`, {
-      method: 'GET',
-      mode: 'cors',
-      credentials: 'include',
-    });
-
-    console.log(response);
-
-    const cookieStore = await cookies();
-    const theme = cookieStore.get('theme');
-
-    console.log(theme);
-
-    // HTTPステータスが 200 でない場合は失敗
-    if (!response.ok) {
-      throw new Error('ログイン状態の取得に失敗しました');
-    }
-
-    return true;
-  } catch (error: unknown) {
-    console.error(error);
+  } catch {
     return false;
   }
 }
