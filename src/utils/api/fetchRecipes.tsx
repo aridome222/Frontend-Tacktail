5fb85434954e22a42e46cf5a24ad9cad5f255c0f
KakinokiKanta <kanta12621@gmail.com>
2024-11-15 08:00:17 +0900

    add: 念のためcors等の設定をfetch関数に追記

--- src/utils/api/fetchRecipes.tsx
+++ src/utils/api/fetchRecipes.tsx
@@ -4,7 +4,10 @@ export const fetchRecipes = async (): Promise<RecipeData[]> => {
   // エンドポイント（TODO: 実行中の環境に応じて、環境変数の部分を本番環境 or 開発環境のものにする）
   const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/cocktails/list`;
 
-  const response = await fetch(url);
+  const response = await fetch(url, {
+    mode: 'cors',
+    credentials: 'include',
+  });
   if (!response.ok) {
     throw new Error(`レスポンスステータス: ${response.status}`);
   }
