22c5f56ad5f77613a44693acf010b139f4860334
KakinokiKanta <kanta12621@gmail.com>
2024-11-15 22:35:41 +0900

    fix: fetchRecipeWithAuth内のエンドポイント誤りを修正

--- src/utils/api/fetchRecipeWithAuth.tsx
+++ src/utils/api/fetchRecipeWithAuth.tsx
@@ -2,7 +2,7 @@ import type { RecipeData } from '../types';
 
 export const fetchRecipeWithAuth = async (id: string, token: string): Promise<RecipeData> => {
   // エンドポイント（TODO: 実行中の環境に応じて、環境変数の部分を本番環境 or 開発環境のものにする）
-  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/cocktails/individual/${id}`;
+  const url = `${process.env.NEXT_PUBLIC_BACKEND_URL}/auth/cocktails/individual/${id}`;
 
   const response = await fetch(url, {
     headers: {
